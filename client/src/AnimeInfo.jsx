6748ee0121e1a7802ccbb6717483aa3a2532c90d
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2025-09-05 19:06:00 -0400

    API request and update code to use the result of the request for anime info

--- client/src/AnimeInfo.jsx
+++ client/src/AnimeInfo.jsx
@@ -1,25 +1,50 @@
 import styles from "./AnimeInfo.module.css";
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import { API_URL } from "./config";
 
 function AnimeInfo() {
     const navigate = useNavigate();
 
+    const [animeInfo, setAnimeInfo] = useState({
+        anime_name: null,
+        img_url: null,
+        author: null,
+        rating: null
+    });
+
+    useEffect(() => {
+        async function fetchData() {
+            const res = await axios.get(`${API_URL}/api/anime-info?id=${localStorage.getItem("id")}`);
+
+            setAnimeInfo(res.data.result);
+        }
+
+        fetchData();
+    }, []);
+
     return (
         <>
-            <button onClick={() => { navigate("/home") }}>
+            <button onClick={() => { navigate("/home"); }}>
                 ←
+            </button>
+
+            <button id={styles.editButton} onClick={() => { navigate(); }}>
+                EDIT
             </button>
 
             <div className={styles.container}>
                 <div className={styles.box} id={styles.imgBox}>
-                    <img height="300" width="200" />
+                    <img src={animeInfo.img_url} height="300" width="200" />
                 </div>
 
                 <div className={styles.box}>
                     <ul id={styles.info}>
-                        <li>Name: </li>
-                        <li>Author: </li>
-                        <li>Rating:</li>
+                        <li>Name: {animeInfo.anime_name}</li>
+                        <li>Author: {animeInfo.author}</li>
+                        <li>Rating: {animeInfo.rating}</li>
                     </ul>
                 </div>
             </div>
