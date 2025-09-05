d9f1e984a7547b900520b0d43a7e938a8a12268b
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2025-09-05 18:46:52 -0400

    API request and update code to use the result of the request

--- client/src/Anime.jsx
+++ client/src/Anime.jsx
@@ -1,23 +1,48 @@
 import styles from "./Anime.module.css";
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import { API_URL } from "./config";
 
 function Anime() {
     const navigate = useNavigate();
 
-    let animeList = ["Sprited Away", "Your Name", "Demon Slayer", "Attack on Titan"];
+    const [animeList, setAnimeList] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const res = await axios.get(`${API_URL}/api/animes`);
+
+                setAnimeList(res.data.results);
+            }
+            catch (err) {
+                console.error("Error:", err);
+            }
+        }
+
+        fetchData();
+    }, []);
 
     return (
         <div className={styles.container}>
-            {animeList.map((anime, index) => {
+            {animeList.map((anime) => {
                 return (
-                    <div key={index}>
-                        <button className={styles.anime} onClick={() => { navigate("/anime-info") }}>
-                            <img />
-                        </button>
+                    <div key={anime.id}>
+                        <button
+                            className={styles.anime}
 
-                        <h2 className={styles.animeTitle}>{anime}</h2>
+                            style={{ backgroundImage: `url("${anime.img_url}")` }}
+
+                            onClick={() => {
+                                localStorage.setItem("id", anime.id);
+                                navigate("/anime-info");
+                            }}></button>
+
+                        <h2 className={styles.animeTitle}>{anime.anime_name}</h2>
                     </div>
-                )
+                );
             })}
         </div>
     );
